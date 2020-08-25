74d79f05956534a4ede1ba9079d42d8d1a148e4e
Tommy Murray <user-pc@djtm.live>
2020-08-25 12:23:49 +0200

    init

---
+++ handeler.js
@@ -0,0 +1,23 @@
+jQuery(function() {
+    function socialTemplate(social) {
+        return `
+        <a href="${social.link}"><img ></img></a>
+        `}
+    jQuery.getJSON('editme.json', function(data){
+        console.log(data)
+        const sitedata = data[0]
+        const socialdata = data[1]
+        const {layout,logo,name}=sitedata
+        if (layout==="logo") {
+            document.getElementById('logotext').innerHTML=`<div class="container" style="padding-top: 20px;"><center><img src="${logo}" alt="${name}'s Logo" class="logo"></center></div>`
+        } else if (layout==="text") {
+            document.getElementById('logotext').innerHTML=`<div class="container" style="padding-top: 20px;"><div class="row"><div class="col-12"><center><h1 style="padding-top: 10px;">${name}</h1></center></div></div></div>`
+            console.log('text');
+        } else if (layout==="logotext") {
+            document.getElementById('logotext').innerHTML=`<div class="container" style="padding-top: 20px;"><div class="row"><div class="col-12"><center><img src="${logo}" alt="${name}'s Logo" class="logo"></center></div><div class="col-12"><center><h1 style="padding-top: 10px;">${name}</h1></center></div></div></div>`
+        }
+        document.getElementById('dynamicsite').innerHTML = `
+        
+        `
+        //document.getElementById('app').textContent = name
+    });});
