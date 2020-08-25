22e0a18ca99acd690079350a7a465c75fa5db2a9
Tommy Murray <user-pc@djtm.live>
2020-08-25 15:43:59 +0200

    removed metatags from handler.js

--- handler.js
+++ handler.js
@@ -21,8 +21,6 @@ jQuery(function() {
     jQuery.getJSON('editme.json', function(data){
         const sitedata = data[0]
         const socialdata = data[2]
-        const metatags = data[3]
-        const {title,description,ogcolor,twitterUsername,keywords,siteurl,ogimage}=metatags
         const {layout,logo,name}=sitedata
         const spanorh1 ="h1"
 
@@ -42,38 +40,4 @@ jQuery(function() {
         ${socialdata.map(hoverColor).join('')}
         `
 
-        document.getElementById('metatags').innerHTML=`
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-        <link rel="stylesheet" href="./style.css">
-    
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="./handeler.js"></script>
-
-        
-        <title>${title}</title>
-        <link rel="icon" type="image/png" href="${logo}" sizes="32x32">
-        <link rel="icon" type="image/png" href="${logo}" sizes="96x96">
-        <link rel="apple-touch-icon" sizes="180x180" href="./assets/apple-touch-icon.png">
-      
-        <meta name="theme-color" content="${ogcolor}">
-        <meta property="og:title" content="${title}">
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="${siteurl}">
-        <meta property="og:description" content="${description}">
-        <meta property="og:image" content="${ogimage}">
-        <meta name="twitter:card" content="summary_large_image">
-      
-        <meta name="description" content="${description}">
-        <meta name="keywords" content="${keywords}">
-      
-        <meta name="twitter:card" content="summary">
-        <meta name="twitter:image" content="${siteurl}${ogimage}">
-        <meta name="twitter:creator" content="${twitterUsername}">
-        
-        `
-
-        //document.getElementById('app').textContent = name
     });});
